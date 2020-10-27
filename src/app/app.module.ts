35f7413d908c81558692bccdf31ea422ee30c551
serhiirodikov <sergiy1998373@gmail.com>
2020-10-27 23:42:07 +0200

    create component with CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent],
+  declarations: [AppComponent, PostComponent, Post2Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
