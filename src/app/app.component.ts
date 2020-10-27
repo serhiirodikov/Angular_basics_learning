24cef4d8288890be2a95330d91c85f7169f883cd
serhiirodikov <sergiy1998373@gmail.com>
2020-10-28 00:12:51 +0200

    binding src example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  img = 'https://learn.javascript.ru/courses/list/angular.png';
+
   title = 'angular-first-touch';
+  constructor() {
+    setTimeout(() => {
+      this.img =
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png';
+    }, 3000);
+  }
 }
