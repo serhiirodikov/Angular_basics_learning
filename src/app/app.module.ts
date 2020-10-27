8673ccf86b04bb75e2a70c7858c37c9a8c08a3aa
serhiirodikov <sergiy1998373@gmail.com>
2020-10-27 23:52:48 +0200

    example 'template and styles'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { Post2Component } from './post2/post2.component';
+import { Post3Component } from './post3/post3.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent, Post2Component],
+  declarations: [AppComponent, PostComponent, Post2Component, Post3Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
