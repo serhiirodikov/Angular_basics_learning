af3fd2ece4684bb13e9e23666d097dee2206eec5
serhiirodikov <sergiy1998373@gmail.com>
2020-10-28 00:46:59 +0200

    some binding test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  img = 'https://learn.javascript.ru/courses/list/angular.png';
-
+  // img = 'https://learn.javascript.ru/courses/list/angular.png';
+  inputValue = '';
   title = 'angular-first-touch';
   constructor() {
-    setTimeout(() => {
-      this.img =
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png';
-    }, 3000);
+    // setTimeout(() => {
+    //   this.img =
+    //     'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png';
+    // }, 3000);
+  }
+  onInput(e?) {
+    this.inputValue = e.target.value;
+  }
+  onClick() {
+    console.log('clicked');
   }
 }
